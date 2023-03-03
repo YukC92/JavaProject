aafe5c09817395f0227b942909f1733a3f9bb628
YukC92 <yukfaichan92@gmail.com>
2023-03-02 23:25:21 -0800

    setting

---
+++ ChatApplication/src/chatting/application/Server.java
@@ -0,0 +1,17 @@
+package chatting.application;
+
+import javax.swing.*;
+
+public class Server extends JFrame{
+
+    Server() {
+        setSize(450,700);
+        setVisible(true);
+        setLocation(200, 50);
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+
+}
